01f31f0c83d9672a66204dfd1bf45bbd26b9e322
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-10 21:57:11 -0300

    Add Main & Test Application

---
+++ src/main/java/com/br/IntegracaoImoveis/IntegacaoImoveisApplication.java
@@ -0,0 +1,13 @@
+package com.br.IntegracaoImoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegacaoImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegacaoImoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/IntegracaoImoveis/IntegacaoImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.IntegracaoImoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegacaoImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
