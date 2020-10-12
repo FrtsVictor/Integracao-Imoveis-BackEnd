74ffdaa0f8835a64f93be0e5bb0c30659a849848
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-11 23:29:49 -0300

    Add findByEmail -finById  /repository UserRepository

--- src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package com.br.IntegracaoImoveis.repository;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.br.IntegracaoImoveis.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	@Query(value = "FROM User WHERE email = ?1")
+		User userByEmail(String email);
+
+	@Query(value = "FROM User WHERE id = ?1")
+	User userById(Long id);
+
+	
 }
