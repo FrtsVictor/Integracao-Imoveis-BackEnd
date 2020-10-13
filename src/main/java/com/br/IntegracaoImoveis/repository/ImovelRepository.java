bed34b56808438f96acabccc8164f3009f29eab2
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-13 00:11:56 -0300

    Hot fix add @Repository /repository Imovel

--- src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
@@ -1,9 +1,12 @@
 package com.br.IntegracaoImoveis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.br.IntegracaoImoveis.model.Imovel;
 
+
+@Repository
 public interface ImovelRepository extends JpaRepository<Imovel, Long> {
 
 }
