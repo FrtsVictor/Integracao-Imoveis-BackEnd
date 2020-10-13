6726144615cc75357f5f1afe3424db23cb758a90
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-12 23:53:13 -0300

    Add roles for admin routes User/model

--- src/main/java/com/br/IntegracaoImoveis/model/User.java
+++ src/main/java/com/br/IntegracaoImoveis/model/User.java
@@ -1,43 +1,99 @@
 package com.br.IntegracaoImoveis.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import javax.persistence.JoinColumn;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "email"),
+		@UniqueConstraint(columnNames = "email") })
 @EntityListeners(AuditingEntityListener.class)
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@Column(nullable = false)
-	@NotBlank(message = "Name should be informed") 
+	@Size(max = 50, message = "Please insert a firtsName less than 50 characters")
+	@NotBlank(message = "Name should be informed")
 	private String firstName;
-	
+
 	@Column(nullable = false)
+	@Size(max = 50, message = "Please insert a lastName less than 50 characters")
 	@NotBlank(message = "lastName should be informed")
 	private String lastName;
-	
+
 	@Column
+	@Size(max = 50, message = "Email invalid, over 50 characters")
 	@Email(message = "Email should be valid")
 	private String email;
-	
-	@Column 
+
+	@Column
 	@Size(min = 5, message = "Password should be at least 5 characters")
 	@NotBlank(message = "lastName should be informed")
 	private String password;
+
+	@NotBlank
+	@Size(max = 20)
+	private String username;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+
+
+	public User(
+			 String firstName,
+			 String lastName,
+			 String email,
+			 String password,
+			String username) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.username = username;
+	}
+	
+	 
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	public User() {
+		super();
+	}
+
+
 
 	public Long getId() {
 		return id;
@@ -79,11 +135,22 @@ public class User {
 		this.password = password;
 	}
 
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", password=" + password + "]";
+				+ ", password=" + password + ", username=" + username + ", roles=" + roles + "]";
 	}
+	
 	
 	
 
