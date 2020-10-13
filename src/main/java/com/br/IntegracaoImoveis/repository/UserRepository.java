5d1c4429d0af4d38c15f66df303e4c06dc5ba0df
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-13 00:10:19 -0300

    Add boolean method verifify if exists /repository UserRepository

--- src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
@@ -2,11 +2,16 @@ package com.br.IntegracaoImoveis.repository;
 
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.br.IntegracaoImoveis.model.User;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query(value = "FROM User WHERE email = ?1")
@@ -15,5 +20,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	@Query(value = "FROM User WHERE id = ?1")
 	User userById(Long id);
 
+	Optional<User> findByUsername(String username);
+	Boolean existsByUsername(String username);
+	Boolean existsByEmail(String email);
 	
 }
