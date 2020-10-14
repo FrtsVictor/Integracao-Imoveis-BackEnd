7c29492fe3c122a80416f394346b6b3ad341f947
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-13 22:49:33 -0300

    Add ResourceNotFoundException refactor validations  /controller User

--- src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
+++ src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
@@ -1,19 +1,18 @@
 package com.br.IntegracaoImoveis.controller;
 
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
-
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,11 +21,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.br.IntegracaoImoveis.exceptions.EmailAlreadyExistsExeception;
-import com.br.IntegracaoImoveis.exceptions.InvalidCredentialException;
+import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundException;
 import com.br.IntegracaoImoveis.model.User;
 import com.br.IntegracaoImoveis.repository.UserRepository;
 import com.br.IntegracaoImoveis.service.UserService;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -52,34 +51,37 @@ public class UserController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<User> getById(@PathVariable Long id) throws Exception {
-		userService.userExists(id);
+		verifyIdUserExists(id);
 		User user = userRepository.userById(id);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
-
 	}
 
-	
+	//used for rollback on unchecked exceptions
 	@PostMapping
-	public ResponseEntity<User> createuser(@Valid @RequestBody User user) throws EmailAlreadyExistsExeception {
-		userService.emailAlreadyExists(user.getEmail());
+	@Transactional(rollbackFor = Exception.class )
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
 		String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10));
 		user.setPassword(hashedPassword);
 		userRepository.save(user);
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
 	}
 
 	
-	@PostMapping( "/login")
-	public ResponseEntity<?> loginUser(@Valid @RequestBody Map<String, Object> userMap) throws InvalidCredentialException {
-		String email = (String) userMap.get("email");
-		String password = (String) userMap.get("password");	
-		User user = userService.validateEmail(email);
-		
-		User authUser = userService.validateLogin(user, password);	
-		Map<String, String> map = new HashMap<>();
-		map.put("message", "LoggedIn sucessfully");
-
-		return new ResponseEntity<Map>(map, HttpStatus.OK);
+	@DeleteMapping("/{id}")
+	public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+		verifyIdUserExists(id);
+		userRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 
 	}
+	
+	
+	private void verifyIdUserExists(Long id) {
+		if (userRepository.userById(id) == null)
+			throw new ResourceNotFoundException("User not Found for id" + id);
+	}
+	
+	
+	
 }
