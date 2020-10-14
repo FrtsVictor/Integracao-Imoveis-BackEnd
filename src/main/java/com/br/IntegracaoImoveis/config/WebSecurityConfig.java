b1fdc900807c9489d8bd4f3723e24642e05ee01b
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-13 22:47:42 -0300

    Add new authorized route  /config WebSecutiryConfig

--- src/main/java/com/br/IntegracaoImoveis/config/WebSecurityConfig.java
+++ src/main/java/com/br/IntegracaoImoveis/config/WebSecurityConfig.java
@@ -60,6 +60,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+			.and()
+			.authorizeRequests().antMatchers("/api/users/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
 
