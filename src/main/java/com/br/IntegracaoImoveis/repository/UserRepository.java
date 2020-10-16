f1482af13dcacbf6aaffea88f9989b4a8b485177
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-16 01:00:08 -0300

    Changed JpaRepo to PagAndSortRepo /repository UserRepository

--- src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.br.IntegracaoImoveis.repository;
 
+import java.util.Optional;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.br.IntegracaoImoveis.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
 	@Query(value = "FROM User WHERE email = ?1")
 		User userByEmail(String email);
@@ -15,5 +18,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	@Query(value = "FROM User WHERE id = ?1")
 	User userById(Long id);
 
+	Optional<User> findByUsername(String username);
+	Boolean existsByUsername(String username);
+	Boolean existsByEmail(String email);
 	
 }
