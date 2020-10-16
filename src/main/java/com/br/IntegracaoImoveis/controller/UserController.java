b8ece5d2f75f0e5ef0bd3959585aeaec4fff7960
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-16 00:54:21 -0300

    Add Pageable GET add admin and user role routes /controller UserController

--- src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
+++ src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
@@ -1,85 +1,82 @@
 package com.br.IntegracaoImoveis.controller;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
-
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.IntegracaoImoveis.exceptions.EmailAlreadyExistsExeception;
-import com.br.IntegracaoImoveis.exceptions.InvalidCredentialException;
+import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundException;
 import com.br.IntegracaoImoveis.model.User;
 import com.br.IntegracaoImoveis.repository.UserRepository;
-import com.br.IntegracaoImoveis.service.UserService;
+
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserController {
-
 
 	@Autowired
 	UserRepository userRepository;
 	
-	@Autowired
-	UserService userService;
-
 	public HttpHeaders Header() {
 		HttpHeaders header = new HttpHeaders();
 		header.add("IntegraçãoImoveis", "REST_API");
 		return header;
 	}
 
-	@GetMapping
-	public ResponseEntity<List<User>> getAllUsers() {
-		return new ResponseEntity<List<User>>(userRepository.findAll(),  HttpStatus.OK);
+	@GetMapping("/users")
+	@Transactional(rollbackFor = Exception.class )
+	public ResponseEntity<?> getAllUsers(@PageableDefault(size = 10) Pageable pageable) {
+		return new ResponseEntity<>(userRepository.findAll(pageable),  HttpStatus.OK);
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<User> getById(@PathVariable Long id) throws Exception {
-		userService.userExists(id);
+	@GetMapping("/users/{id}")
+	@Transactional(rollbackFor = Exception.class )
+	public ResponseEntity<User> getById(@Valid @PathVariable Long id) throws Exception {
+		verifyIdUserExists(id);
 		User user = userRepository.userById(id);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
-
 	}
 
-	
-	@PostMapping
-	public ResponseEntity<User> createuser(@Valid @RequestBody User user) throws EmailAlreadyExistsExeception {
-		userService.emailAlreadyExists(user.getEmail());
+	@PostMapping("/sign-up")
+	@Transactional(rollbackFor = Exception.class )
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) throws EmailAlreadyExistsExeception{
 		String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10));
 		user.setPassword(hashedPassword);
 		userRepository.save(user);
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
 	}
 
 	
-	@PostMapping( "/login")
-	public ResponseEntity<?> loginUser(@Valid @RequestBody Map<String, Object> userMap) throws InvalidCredentialException {
-		String email = (String) userMap.get("email");
-		String password = (String) userMap.get("password");	
-		User user = userService.validateEmail(email);
-		
-		User authUser = userService.validateLogin(user, password);	
-		Map<String, String> map = new HashMap<>();
-		map.put("message", "LoggedIn sucessfully");
-
-		return new ResponseEntity<Map>(map, HttpStatus.OK);
-
+	@DeleteMapping("/admin/users/{id}")
+	@Transactional(rollbackFor = Exception.class )
+	public ResponseEntity<User> deleteUser(@Valid @PathVariable Long id) {
+		verifyIdUserExists(id);
+		userRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
+	
+	
+	private void verifyIdUserExists(Long id) {
+		if (userRepository.userById(id) == null)
+			throw new ResourceNotFoundException("User not Found for id" + id);
+	}
+	
+	
+	
 }
