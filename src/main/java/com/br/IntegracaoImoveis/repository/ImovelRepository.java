449fe85ff943c0e60e68e458c959c2a58e92eeab
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-16 01:01:34 -0300

    Changed ImovelRepo do PagAndSortRepo /repository ImovelRepo

--- src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
@@ -1,9 +1,11 @@
 package com.br.IntegracaoImoveis.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.br.IntegracaoImoveis.model.Imovel;
 
-public interface ImovelRepository extends JpaRepository<Imovel, Long> {
+@Repository
+public interface ImovelRepository extends PagingAndSortingRepository<Imovel, Long> {
 
 }
