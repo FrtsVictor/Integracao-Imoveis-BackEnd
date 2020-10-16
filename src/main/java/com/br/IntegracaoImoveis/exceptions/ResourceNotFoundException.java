63c5cf4d9021a1867e87b9c88c6a902535c36b7e
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-16 01:09:05 -0300

    Add ResouceNotFoundException  /exceptions

---
+++ src/main/java/com/br/IntegracaoImoveis/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,23 @@
+package com.br.IntegracaoImoveis.exceptions;
+
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 4097220015877199455L;
+	String msg;
+
+	public ResourceNotFoundException(String msg) {
+		super();
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+	
+}
