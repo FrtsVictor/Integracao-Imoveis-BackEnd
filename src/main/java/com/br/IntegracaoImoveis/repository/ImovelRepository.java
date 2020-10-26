e1244698acdbf2ad5e091d5c7148896f3358f7cd
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-25 23:29:01 -0300

    Add Query for imovelByUsername /repository ImovelRepository

--- src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
@@ -1,5 +1,10 @@
 package com.br.IntegracaoImoveis.repository;
 
+
+
+import java.awt.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +12,11 @@ import com.br.IntegracaoImoveis.model.Imovel;
 
 @Repository
 public interface ImovelRepository extends PagingAndSortingRepository<Imovel, Long> {
+	
+	
 
+	@Query(value = "FROM Imovel WHERE idUsuario = ?1")
+	List imovelByUsername(Long idUsuario);
+	
+	
 }
