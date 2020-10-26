7389d4db6fa9eb7b55faee8a00dd934b38c6023a
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-25 23:24:32 -0300

    Add GetByUserName route  /controller UserController

--- src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
+++ src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
@@ -2,6 +2,8 @@ package com.br.IntegracaoImoveis.controller;
 
 
 
+import java.util.Set;
+
 import javax.validation.Valid;
 
 import org.mindrot.jbcrypt.BCrypt;
@@ -12,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,18 +23,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.br.IntegracaoImoveis.exceptions.EmailAlreadyExistsExeception;
 import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundException;
+import com.br.IntegracaoImoveis.model.Imovel;
 import com.br.IntegracaoImoveis.model.User;
 import com.br.IntegracaoImoveis.repository.UserRepository;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
 	
 	public HttpHeaders Header() {
 		HttpHeaders header = new HttpHeaders();
@@ -52,10 +55,30 @@ public class UserController {
 		User user = userRepository.userById(id);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
-
+	
+	@GetMapping("/username/{username}")
+	public ResponseEntity<?> getByUsername(@PathVariable String username) {
+		User user = userRepository.userByUsername(username);
+		user.setPassword("secret");
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+	
+	
+	@PostMapping("/add/{id}")
+	public ResponseEntity<?> addImovel(
+									@PathVariable Long id,
+									@RequestBody Imovel imovel) {
+		User user = userRepository.userById(id);
+		Set lists = user.getImovelFavorito();
+		lists.add(imovel);
+		userRepository.save(user);
+		return new ResponseEntity<>(lists, HttpStatus.CREATED);
+	}
+	
+	
 	@PostMapping("/sign-up")
 	@Transactional(rollbackFor = Exception.class )
-	public ResponseEntity<User> createUser(@Valid @RequestBody User user) throws EmailAlreadyExistsExeception{
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
 		String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10));
 		user.setPassword(hashedPassword);
 		userRepository.save(user);
@@ -65,16 +88,16 @@ public class UserController {
 	
 	@DeleteMapping("/admin/users/{id}")
 	@Transactional(rollbackFor = Exception.class )
-	public ResponseEntity<User> deleteUser(@Valid @PathVariable Long id) {
+	public ResponseEntity<Void> deleteUser(@Valid @PathVariable Long id) {
 		verifyIdUserExists(id);
 		userRepository.deleteById(id);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return  ResponseEntity.noContent().build();
 	}
 	
 	
 	private void verifyIdUserExists(Long id) {
 		if (userRepository.userById(id) == null)
-			throw new ResourceNotFoundException("User not Found for id" + id);
+			throw new ResourceNotFoundException("Usuario nao encontrado para id" + id);
 	}
 	
 	
