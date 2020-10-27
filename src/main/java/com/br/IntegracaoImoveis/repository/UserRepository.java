3909219763264d6e8e81e8490fcd41f9efe7c41f
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-27 15:45:47 -0300

    Add @Querys pagination /repository UserRepository

--- src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.br.IntegracaoImoveis.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import com.br.IntegracaoImoveis.model.Imovel;
 import com.br.IntegracaoImoveis.model.User;
 
 
@@ -18,14 +22,20 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	@Query(value = "FROM User WHERE id = ?1")
 	User userById(Long id);
 
+	@Query(value = "FROM User WHERE username = ?1")
+	User userByUsername(String username);
+
+		
 	Optional<User> findByUsername(String username);
-	
 	Boolean existsByUsername(String username);
 	Boolean existsByEmail(String email);
 
-	@Query(value = "FROM User WHERE username = ?1")
-	User userByUsername(String username);
 	
-		
+	Page<?>findAllById(Long id, Pageable pageable);	
+	
+	Page<?>findAllByImoveis(Pageable pageable);
+	
+//	@Query(value = "select i.* from imoveis_favoritos f join imoveis i on i.id = f.imoveis_id where f.usuarios_id = ?1",  nativeQuery = true)
+//	Page<?>usuarioImoveis(Long id, Pageable pageable);
 	
 }
