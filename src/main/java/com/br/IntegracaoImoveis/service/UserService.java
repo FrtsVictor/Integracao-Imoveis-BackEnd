2fee39a6745af73eada17449821c45e11ac3576e
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-28 10:51:56 -0300

    Add UserService /service

--- src/main/java/com/br/IntegracaoImoveis/service/UserService.java
+++ src/main/java/com/br/IntegracaoImoveis/service/UserService.java
@@ -1,54 +1,59 @@
 package com.br.IntegracaoImoveis.service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import org.mindrot.jbcrypt.BCrypt;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import com.br.IntegracaoImoveis.exceptions.EmailAlreadyExistsExeception;
-import com.br.IntegracaoImoveis.exceptions.InvalidCredentialException;
-import com.br.IntegracaoImoveis.exceptions.InvalidQueryException;
-import com.br.IntegracaoImoveis.exceptions.UserNotExistsException;
+import com.br.IntegracaoImoveis.DTO.UserDTO;
+import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundException;
 import com.br.IntegracaoImoveis.model.User;
 import com.br.IntegracaoImoveis.repository.UserRepository;
 
+@Component
 @Service
 public class UserService {
 
-	
+	@Autowired
+	ModelMapper modelMapper;
+
 	@Autowired
 	UserRepository userRepository;
+	
 
-	
-	public User validateEmail(String email) throws InvalidCredentialException {
-		User user = userRepository.userByEmail(email);
-		if(user == null) {
-			throw new InvalidCredentialException("Invalid user");
+	public UserDTO DTOtoModel(User usr) {
+		return modelMapper.map(usr, UserDTO.class);
+	}
+
+	public List<UserDTO> toCollectionDTO(Set<User> userList) {
+		Set<UserDTO> dtoSet = new HashSet<>();
+		for (User usr : userList) {
+			UserDTO usrDTO = DTOtoModel(usr);
+			dtoSet.add(usrDTO);
 		}
-		return user;
+		List<UserDTO> dtoList = new ArrayList<>(dtoSet);
+		return dtoList;
 	}
-	
-	
-	public User validateLogin(User user, String password) throws InvalidCredentialException {
-		System.out.println(BCrypt.checkpw(password, user.getPassword()));
-		if(!BCrypt.checkpw(password, user.getPassword())) {
-			throw new InvalidCredentialException("Login or Password Wrong");
-		}
-		return user;
+
+	public Page<UserDTO> cretePage(Pageable pageable, List<UserDTO> dtoList) {
+		int start = (int) pageable.getOffset();
+		int end = (start + pageable.getPageSize()) > dtoList.size() ? dtoList.size() : (start + pageable.getPageSize());
+
+		Page<UserDTO> pages = new PageImpl<UserDTO>(dtoList.subList(start, end), pageable, dtoList.size());
+		return pages;
 	}
-	
-	
-	public void userExists(Long id) throws InvalidQueryException, UserNotExistsException {
-		if(id < 0)throw new InvalidQueryException("ID cant be less than 0", id); 
-		User user = userRepository.userById(id);
-		if(user == null) throw new UserNotExistsException("Not found any user with this this ID", id);
+
+	public void userExists(Long id) {
+		if (userRepository.userById(id) == null)
+			throw new ResourceNotFoundException("Usuario nao encontrado para id " + id);
 	}
-	
-	
-	public void emailAlreadyExists(String email) throws EmailAlreadyExistsExeception {		
-		User user = userRepository.userByEmail(email);
-		if(user !=null) throw new EmailAlreadyExistsExeception("Email already in use", email);
-	} 
-	
-	
+
 }
