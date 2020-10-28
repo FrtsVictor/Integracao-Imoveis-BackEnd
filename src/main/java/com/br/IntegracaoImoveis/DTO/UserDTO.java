fd9bd89a810487fda7014de70e1972f2beb0c166
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-28 10:45:56 -0300

    Add new attributes to /DTO UserDTO

--- src/main/java/com/br/IntegracaoImoveis/DTO/UserDTO.java
+++ src/main/java/com/br/IntegracaoImoveis/DTO/UserDTO.java
@@ -1,56 +1,67 @@
 package com.br.IntegracaoImoveis.DTO;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.br.IntegracaoImoveis.model.Imovel;
-
 public class UserDTO {
 
-	private String nome;
-	private	Set<ImovelDTO> imoveis = new HashSet<>();
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public Set<ImovelDTO> getImoveis() {
-		return imoveis;
-	}
-	public void setImoveis(Set<ImovelDTO> imoveis) {
-		this.imoveis = imoveis;
-	}
-	public UserDTO(String nome, Set<ImovelDTO> imoveis) {
-		super();
-		this.nome = nome;
-		this.imoveis = imoveis;
-	}
+	private Long id;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String email;
 	
 	
-	
-	@Autowired
-	private ModelMapper modelMapper;
-	
+	public UserDTO() {}
 
-	private ImovelDTO DTOtoModel(Imovel imovel) {
-		return modelMapper.map(imovel, ImovelDTO.class); 
+
+	public Long getId() {
+		return id;
 	}
 
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
-	private Set<ImovelDTO> toCollectionDTO (Set<Imovel> lista){
-		Set<ImovelDTO> lista2 = new HashSet<>();		 
-		 for(Imovel imv : lista) {
-			 ImovelDTO imvs = DTOtoModel(imv);
-			 lista2.add(imvs);
-		 }
-		 return lista2;
-	}	
+	
 	
 	
 }
