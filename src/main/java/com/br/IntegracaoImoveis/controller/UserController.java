f49338f392879a4893489e4ce80d64eaf1a3cec2
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-28 10:44:49 -0300

    Cleaning code and add pagination /controller UserController

--- src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
+++ src/main/java/com/br/IntegracaoImoveis/controller/UserController.java
@@ -2,14 +2,17 @@ package com.br.IntegracaoImoveis.controller;
 
 
 
+import java.util.List;
+import java.util.Set;
+
 import javax.validation.Valid;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,9 +24,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.IntegracaoImoveis.DTO.UserDTO;
 import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundException;
 import com.br.IntegracaoImoveis.model.User;
 import com.br.IntegracaoImoveis.repository.UserRepository;
+import com.br.IntegracaoImoveis.service.UserService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -33,6 +38,10 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	
+	@Autowired
+	UserService userService;
+	
 	public HttpHeaders Header() {
 		HttpHeaders header = new HttpHeaders();
 		header.add("IntegraçãoImoveis", "REST_API");
@@ -41,50 +50,47 @@ public class UserController {
 
 	@GetMapping("/users")
 	@Transactional(rollbackFor = Exception.class )
-	public ResponseEntity<?> getAllUsers(@PageableDefault(size = 10) Pageable pageable) {
-		return new ResponseEntity<>(userRepository.findAll(pageable),  HttpStatus.OK);
+	public ResponseEntity<Page<UserDTO>> getAllUsers(@PageableDefault(size = 10) Pageable pageable) {
+		Set<User> userSet = userRepository.findAll();
+		List<UserDTO> usersDTO = userService.toCollectionDTO(userSet);
+		return ResponseEntity.ok(userService.cretePage(pageable, usersDTO));	
+	
 	}
 
 	
 	@GetMapping("/users/{id}")
 	@Transactional(rollbackFor = Exception.class )
-	public ResponseEntity<User> getById(@Valid @PathVariable Long id) throws Exception {
-		verifyIdUserExists(id);
-		User user = userRepository.userById(id);
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+	public ResponseEntity<UserDTO> getById(@Valid @PathVariable Long id) throws Exception {
+		userService.userExists(id);
+		UserDTO userDTO = userService.DTOtoModel(userRepository.userById(id));
+		return ResponseEntity.ok(userDTO);
 	}
 	
 	
 	@GetMapping("/username/{username}")
-	public ResponseEntity<?> getByUsername(@PathVariable String username) {
-		User user = userRepository.userByUsername(username);
-		user.setPassword("secret");
-		return new ResponseEntity<>(user, HttpStatus.OK);
+	public ResponseEntity<UserDTO> getByUsername(@PathVariable String username) {
+		User user = userRepository.userByUsername(username)
+				.orElseThrow(()-> new ResourceNotFoundException("Username invalid"));
+		return ResponseEntity.ok(userService.DTOtoModel(user));
 	}
 	
 		
 	@PostMapping("/sign-up")
 	@Transactional(rollbackFor = Exception.class )
-	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+	public ResponseEntity<UserDTO> createUser(@Valid @RequestBody User user){
 		String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10));
 		user.setPassword(hashedPassword);
 		userRepository.save(user);
-		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+		return ResponseEntity.ok(userService.DTOtoModel(user));
 	}
 
 	
 	@DeleteMapping("/admin/users/{id}")
 	@Transactional(rollbackFor = Exception.class )
 	public ResponseEntity<Void> deleteUser(@Valid @PathVariable Long id) {
-		verifyIdUserExists(id);
+		userService.userExists(id);
 		userRepository.deleteById(id);
 		return  ResponseEntity.noContent().build();
-	}
-	
-	
-	private void verifyIdUserExists(Long id) {
-		if (userRepository.userById(id) == null)
-			throw new ResourceNotFoundException("Usuario nao encontrado para id" + id);
 	}
 	
 	
