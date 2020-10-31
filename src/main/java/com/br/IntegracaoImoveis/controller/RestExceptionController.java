34906f3eebfd77eb5a78ca755a2ca8c0539bd006
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-30 22:05:47 -0300

    Updating builder

--- src/main/java/com/br/IntegracaoImoveis/controller/RestExceptionController.java
+++ src/main/java/com/br/IntegracaoImoveis/controller/RestExceptionController.java
@@ -1,9 +1,8 @@
 package com.br.IntegracaoImoveis.controller;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -13,16 +12,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.br.IntegracaoImoveis.exceptions.ErrorDetailsPattern;
+import com.br.IntegracaoImoveis.exceptions.ImovelAlreadyLikedException;
+import com.br.IntegracaoImoveis.exceptions.Problem;
 import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundDetails;
 import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundException;
-import com.br.IntegracaoImoveis.exceptions.ValidationErrorDetails;
 
 @ControllerAdvice
 public class RestExceptionController extends ResponseEntityExceptionHandler  {
@@ -35,60 +38,52 @@ public class RestExceptionController extends ResponseEntityExceptionHandler  {
 @ExceptionHandler(ResourceNotFoundException.class)
 public ResponseEntity<?> handlerResourceNotFoundException(ResourceNotFoundException rnfException){
 	ResourceNotFoundDetails rnfdetails = ResourceNotFoundDetails.Builder
-	.newBuilder()
-	.timestamp(new Date().getTime())
-	.status(HttpStatus.NOT_FOUND.value())
-	.title("Busca Nao Encontrada")
-	.detail(rnfException.getMsg())
-	.developerMessage(rnfException.getClass().getName())
-	.build();
-	
+			.newBuilder()
+			.timestamp(OffsetDateTime.now())
+			.status(HttpStatus.NOT_FOUND.value())
+			.title("Busca Nao Encontrada")
+			.detail(rnfException.getMsg())
+			.developerMessage(rnfException.getClass().getName())
+			.build();
 	return new ResponseEntity<>(rnfdetails, HttpStatus.NOT_FOUND);
 }
 
 
+@Override
+protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+		HttpHeaders headers, HttpStatus status, WebRequest request) {
+	ArrayList<com.br.IntegracaoImoveis.exceptions.Problem.Campo> campos = new ArrayList<Problem.Campo>();
+	
+	for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+	String nome = ((FieldError) error).getField();
+		String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+		campos.add(new Problem.Campo(nome,mensagem));
+	}
+	
+	Problem problem = Problem.builder()
+			.status(status.value())
+			.titulo("Um ou mais campos invalidos")
+			.dataHora(OffsetDateTime.now())
+			.campos(campos)
+			.build();
+	return super.handleExceptionInternal(ex, problem, headers, status, request);
+}   
+
+
    @Override
-   public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manvException,
-                                                         HttpHeaders headers,
-                                                         HttpStatus status,
-                                                         WebRequest request) {
-       
-       Map<String, String> errors = new HashMap<>();
-       
-       
-       manvException.getBindingResult().getFieldErrors().forEach(error -> 
-           errors.put(error.getField(), messageSource.getMessage(error, LocaleContextHolder.getLocale())));	
-       
-//       manvException.getBindingResult().getFieldErrors().forEach(error -> 
-//       errors.put(error.getField(), error.getDefaultMessage()));	
-//       
-       
-       ValidationErrorDetails rnfDetails = ValidationErrorDetails.Builder
-               .newBuilder()
-               .timestamp(new Date().getTime())
-               .status(HttpStatus.BAD_REQUEST.value())
-               .title("Error: Validacao dos campos")
-               .detail("BAD REQUEST")
-               .developerMessage(manvException.getClass().getName())
-               .resume(errors)
-               .build();
-       return new ResponseEntity<>(rnfDetails, HttpStatus.BAD_REQUEST);
-   }
-      
-   
-   @Override
-   protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body,
-		   														HttpHeaders headers,
-		   														HttpStatus status,
-		   														WebRequest request) {
+   protected ResponseEntity<Object> handleExceptionInternal(Exception ex, 
+		   	@Nullable Object body,
+		   	HttpHeaders headers,
+		   	HttpStatus status,
+		   	WebRequest request) {
 	   ErrorDetailsPattern errorDetails = ResourceNotFoundDetails.Builder
-			   .newBuilder()
-				.timestamp(new Date().getTime())
-				.status(status.value())
-				.title("Internal Exception")
-				.detail(ex.getMessage())
-				.developerMessage(ex.getClass().getName())
-				.build();
+		    .newBuilder()
+			.timestamp(OffsetDateTime.now())
+			.status(status.value())
+			.title("Internal Exception")
+			.detail(ex.getMessage())
+			.developerMessage(ex.getClass().getName())
+			.build();
 		return new ResponseEntity<>(errorDetails, headers, status);
 	}
 
@@ -96,13 +91,13 @@ public ResponseEntity<?> handlerResourceNotFoundException(ResourceNotFoundExcept
    @ExceptionHandler(Exception.class)
    public final ResponseEntity<ErrorDetailsPattern> handleAllExceptions(Exception ex,  WebRequest request)  {
 	   ErrorDetailsPattern errorDetails = ErrorDetailsPattern.Builder
-			   .newBuilder()
-			   .timestamp(new Date().getTime())
-			   .detail(ex.getMessage())
-			   .developerMessage(ex.getClass().getName())
-			   .status(500)
-			   .title("Internal Server Error")
-			   .build(); 
+		   .newBuilder()
+		   .timestamp(OffsetDateTime.now())
+		   .detail(ex.getMessage())
+		   .developerMessage(ex.getClass().getName())
+		   .status(500)
+		   .title("Internal Server Error")
+		   .build(); 
 	   return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
    }
    
@@ -110,13 +105,13 @@ public ResponseEntity<?> handlerResourceNotFoundException(ResourceNotFoundExcept
    @ExceptionHandler(RuntimeException.class)
    public final ResponseEntity<ErrorDetailsPattern> handleRuntimeException(Exception ex,  WebRequest request)  {
 	   ErrorDetailsPattern errorDetails = ErrorDetailsPattern.Builder
-			   .newBuilder()
-			   .timestamp(new Date().getTime())
-			   .detail(ex.getMessage())
-			   .developerMessage(ex.getClass().getName())
-			   .status(500)
-			   .title("Internal Server Error")
-			   .build(); 
+		   .newBuilder()
+		   .timestamp(OffsetDateTime.now())
+		   .detail(ex.getMessage())
+		   .developerMessage(ex.getClass().getName())
+		   .status(500)
+		   .title("Internal Server Error")
+		   .build(); 
 	   return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
    }
    
@@ -124,35 +119,50 @@ public ResponseEntity<?> handlerResourceNotFoundException(ResourceNotFoundExcept
    @ExceptionHandler(IOException.class)
    public final ResponseEntity<ErrorDetailsPattern> handleIOException(IOException ex,  WebRequest request)  {
 	   ErrorDetailsPattern errorDetails = ErrorDetailsPattern.Builder
-			   .newBuilder()
-			   .timestamp(new Date().getTime())
-			   .detail(ex.getCause().getMessage())
-			   .developerMessage(ex.getClass().getName())
-			   .status(500)
-			   .title("Internal Server Error")
-			   .build(); 
+		   .newBuilder()
+		   .timestamp(OffsetDateTime.now())
+		   .detail(ex.getCause().getMessage())
+		   .developerMessage(ex.getClass().getName())
+		   .status(500)
+		   .title("Internal Server Error")
+		   .build(); 
 	   return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
    }
    
+   
+   
    @ExceptionHandler(DataIntegrityViolationException.class)
-   public final ResponseEntity<ErrorDetailsPattern> handleConstraintViolation(DataIntegrityViolationException ex,
-		   																WebRequest request)  {
-	   System.out.println("message_ " + ex.getLocalizedMessage());
-	   System.out.println("message_ " + ex.getMessage());
-	   System.out.println("cause_ " + ex.getCause());
-	   System.out.println("class_ " + ex.getClass());
-	   System.out.println("trace_ " + ex.getStackTrace());
-	   System.out.println("root_ " + ex.getRootCause());
-	   
+   public final ResponseEntity<ErrorDetailsPattern> handleConstraintViolation(
+		   DataIntegrityViolationException ex,
+		   WebRequest request)  {	   
 	   ErrorDetailsPattern errorDetails = ErrorDetailsPattern.Builder
-			   .newBuilder()
-			   .timestamp(new Date().getTime())
-			   .detail(ex.getRootCause().getMessage())
-			   .developerMessage(ex.getClass().getName())
-			   .status(401)
-			   .title(ex.getCause().getMessage())
-			   .build(); 
+		   .newBuilder()
+		   .timestamp(OffsetDateTime.now())
+		   .detail(ex.getRootCause().getMessage())
+		   .developerMessage(ex.getClass().getName())
+		   .status(401)
+		   .title(ex.getCause().getMessage())
+		   .build(); 
 	   return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
    }
         
+
+   @ResponseStatus(HttpStatus.CONFLICT)
+   @ExceptionHandler(ImovelAlreadyLikedException.class)
+   public final ResponseEntity<ErrorDetailsPattern> handleImovelAlreadyLiked (ImovelAlreadyLikedException imv){
+	   String msg = String.format(imv.getMsg() + imv.getId()); 
+	  
+	   ErrorDetailsPattern errorDetails = ErrorDetailsPattern.Builder
+		   .newBuilder()
+		   .timestamp(OffsetDateTime.now())
+		   .detail(msg)
+		   .status(409)
+		   .title("CONFLICT")
+		   .build(); 
+	   return new ResponseEntity<ErrorDetailsPattern>(errorDetails, HttpStatus.CONFLICT);
+   }
+   
+    
+   
+   
 }
