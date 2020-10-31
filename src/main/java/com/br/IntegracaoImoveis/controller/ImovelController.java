7106d33e8a20ed0e6a147cde599d3166e0713457
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-30 21:54:46 -0300

    Add filtred endpoint /controller

--- src/main/java/com/br/IntegracaoImoveis/controller/ImovelController.java
+++ src/main/java/com/br/IntegracaoImoveis/controller/ImovelController.java
@@ -12,24 +12,26 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.br.IntegracaoImoveis.DTO.ImovelDTO;
 import com.br.IntegracaoImoveis.exceptions.ResourceNotFoundException;
 import com.br.IntegracaoImoveis.model.Imovel;
 import com.br.IntegracaoImoveis.model.User;
+import com.br.IntegracaoImoveis.repository.CustomRepository;
 import com.br.IntegracaoImoveis.repository.ImovelRepository;
 import com.br.IntegracaoImoveis.repository.UserRepository;
 import com.br.IntegracaoImoveis.service.ImovelService;
 
 
 @RestController
+@CrossOrigin(value  = "*")
 @RequestMapping("/api/imoveis")
 public class ImovelController {
 	
@@ -39,19 +41,23 @@ public class ImovelController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired 
+	CustomRepository userCustomRepository;
+	
 	@Autowired ImovelService imovelService;
 	
-
-	@GetMapping("/users")
+	@GetMapping()
 	@Transactional(rollbackFor = Exception.class )	
-	public ResponseEntity<?> getAllUsers(@PageableDefault(size = 6) Pageable pageable) {
+	public ResponseEntity<Page<Imovel>> getAllUsers(@PageableDefault(size = 6)
+													Pageable pageable) {
 		return new ResponseEntity<>(imovelRepository.findAll(pageable),  HttpStatus.OK);
 	}
 	
 
 	@GetMapping("/dto")
 	@Transactional(rollbackFor = Exception.class )	
-	public ResponseEntity<Page<ImovelDTO>> listAllImoveis(@PageableDefault(size = 6) Pageable pageable){
+	public ResponseEntity<Page<ImovelDTO>> listAllImoveis(@PageableDefault(size = 6) 
+														  Pageable pageable){
 		Set<Imovel> imvs = imovelRepository.findAll();			
 		List<ImovelDTO> imvsDTO = imovelService.toDTOList(imvs);		
 		return ResponseEntity.ok(imovelService.cretePage(pageable, imvsDTO));
@@ -63,7 +69,7 @@ public class ImovelController {
 	public ResponseEntity<ImovelDTO> getById(@Valid @PathVariable Long id) throws Exception {
 		Imovel imv = imovelRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Imovel não encontrado para id" + id));
-		return ResponseEntity.ok(imovelService.toDTOClass(imv));	
+		return ResponseEntity.ok(imovelService.imovelToDTO(imv));	
 	}
 	
 
@@ -76,30 +82,32 @@ public class ImovelController {
 		User user = userRepository.userById(id);
 		List<ImovelDTO> imvsDTO = imovelService.toDTOList(user.getImoveis());
 		return ResponseEntity.ok(imovelService.cretePage(pageable, imvsDTO));
-	
 	}
 	
-	@PutMapping("/user/remove/{id}")
+	
+	@PostMapping("/user/{userID}/remove/{imovelID}")
 	@Transactional(rollbackFor = Exception.class )	
-	public ResponseEntity<@Valid Imovel> removeImovel(@Valid @PathVariable Long idUser, 
-													@Valid @PathVariable Long idImovel){
-		imovelService.verifyUser(idUser);
-		User user = userRepository.userById(idUser);		
-		Imovel imv = imovelRepository.findById(idImovel)
-				.orElseThrow(()-> new ResourceNotFoundException("Imovel nao encontrado para id " + idImovel));
+	public ResponseEntity<@Valid Imovel> removeImovel(@Valid @PathVariable Long userID, 
+													  @Valid @PathVariable Long imovelID){
+		imovelService.verifyUser(userID);
+		User user = userRepository.userById(userID);		
+		Imovel imv = imovelRepository.findById(imovelID)
+				.orElseThrow(()-> new ResourceNotFoundException("Imovel nao encontrado para id " + imovelID));
 		user.removeImovel(imv);
 		userRepository.save(user);
 		return ResponseEntity.ok(imv);
 	}
 
-	@PostMapping("/user/{id}")
+	
+	@PostMapping("/user/{userID}")
 	@Transactional(rollbackFor = Exception.class )	
-	public ResponseEntity<@Valid Imovel> addImovel(@Valid @PathVariable Long id, 
-										@Valid @RequestBody Imovel imovel){
-		imovelService.verifyUser(id);
-		User user = userRepository.userById(id);
-		
+	public ResponseEntity<@Valid Imovel> addImovel(@Valid @PathVariable Long userID, 
+												   @Valid @RequestBody Imovel imovel){
+		imovelService.verifyUser(userID);
+		User user = userRepository.userById(userID);
 		Imovel imvRequest = imovelRepository.findByIdImobile(imovel.getIdImobile());
+		
+		imovelService.verifyLikedImovel(user, imvRequest);
 		
 		if (imvRequest != null) {
 			imvRequest.addUser(user);
@@ -110,5 +118,29 @@ public class ImovelController {
 		}
 		return ResponseEntity.ok(imovel);
 	}
+	
+		
+	@GetMapping("/all/{userID}")
+	@Transactional(rollbackFor = Exception.class )	
+	public ResponseEntity<List<Imovel>> getFiltred(
+				@PathVariable Long userID,
+				@RequestParam(value = "cidade", required = false)String cidade,
+				@RequestParam(value = "bairro", required = false) String bairro,
+				@RequestParam(value = "tipo", required = false) String tipo
+			){
+		imovelService.verifyUser(userID);
+		 List<Imovel> imvs = userCustomRepository.find( userID, cidade, bairro, tipo);
+		 return ResponseEntity.ok(imvs); 	
+	}
+	
+	
+	@GetMapping("/bairros/{userID}")
+	@Transactional(rollbackFor = Exception.class )
+	public ResponseEntity<?> findBairros( @PathVariable Long userID){
+		imovelService.verifyUser(userID);
+		 Set<?> imvs = imovelRepository.findAllCidadeBairro(userID);
+		return ResponseEntity.ok(imvs);
+	}
+	
 	
 }
