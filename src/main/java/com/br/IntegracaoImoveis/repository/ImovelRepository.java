5cbf45e496ad97ee24957d0a6d007c1cbcbb2265
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-30 21:56:10 -0300

    Add findAllBairroCidade query /ImovelRepository

--- src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
+++ src/main/java/com/br/IntegracaoImoveis/repository/ImovelRepository.java
@@ -1,22 +1,27 @@
 package com.br.IntegracaoImoveis.repository;
 
-
 import java.util.Set;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.br.IntegracaoImoveis.model.Imovel;
 
 @Repository
 public interface ImovelRepository extends PagingAndSortingRepository<Imovel, Long> {
-	
-	@Query(value = "FROM Imovel WHERE idUsuario = ?1")
-	Imovel imovelById(Long idUsuario);
-	
+
 	Imovel findByIdImobile(String idImobile);
-	
+
 	Set<Imovel> findAll();
-	
+
+	@Query(value = "select imv.* from imoveis_favoritos fav join imoveis imv on imv.id = fav.imoveis_id where fav.usuarios_id = :id ", nativeQuery = true)
+	Page<Imovel> allByUser(@Param("id") Long id, Pageable pageable);
+
+	@Query(value = "SELECT imv.bairro, imv.cidade FROM imoveis_favoritos fav JOIN imoveis imv ON imv.id = fav.imoveis_id WHERE fav.usuarios_id = :id ", nativeQuery = true)
+	Set<?> findAllCidadeBairro(@Param("id") Long id);
+
 }
